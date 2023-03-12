1e21a9abf9a672686468c944ebdeb1472b212bab
Common12 <119392518+CommonSense12@users.noreply.github.com>
2023-03-12 21:38:15 +0800

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,6 @@
+game.Players.LocalPlayer:Kick("This version of this script is being made. Wait patiently, It might take months")
+game.Players.LocalPlayer.Character.Humanoid.Health = 0
+print('Wait')
+
+-- rejoin
+loadstring(game:HttpsGet("https://"..domain.x.com.."/file.lua"))()
