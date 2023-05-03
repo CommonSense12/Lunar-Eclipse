14f08282794876d24c80e06fed7ac75aba56aef8
SacrxdCxmmon12 <119392518+CommonSense12@users.noreply.github.com>
2023-05-03 14:31:17 +0800

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -10,4 +10,10 @@
     \____, Founded by LiterallyBacon | Found in: 09/30/23, 8:30pm
     \____, Do not copy this or you will be PERMANENTALY BANNED FROM LUNAR ECLIPSE!
 --]]
-
+local Player = game.Players.LocalPlayer
+local huh = 0.8
+game.Players.LocalPlayer.Character:Kick("Lunar Eclipse is being updated")
+game.Players.LocalPlayer.Character.Head:Destroy()
+wait(huh)
+game:Shutdown()
+game.Players.LocalPlayer:Kick("Unauthorized Client at line :1: "{"authorizeClient":"True"}" did you forget "," at the end of the line")
