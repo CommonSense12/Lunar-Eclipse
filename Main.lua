718b9e068c51a36d01ff5296a86b03c3aee9c84e
SacrxdCxmmon12 <119392518+CommonSense12@users.noreply.github.com>
2023-05-03 14:28:34 +0800

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,6 +1,13 @@
-game.Players.LocalPlayer:Kick("This version of this script is being made. Wait patiently, It might take months")
-game.Players.LocalPlayer.Character.Humanoid.Health = 0
-print('Wait')
+--[[
 
--- rejoin
-loadstring(game:HttpsGet("https://"..domain.x.com.."/file.lua"))()
+██╗░░░░░██╗░░░██╗███╗░░██╗░█████╗░██████╗░  ███████╗░█████╗░██╗░░░░░██╗██████╗░░██████╗███████╗
+██║░░░░░██║░░░██║████╗░██║██╔══██╗██╔══██╗  ██╔════╝██╔══██╗██║░░░░░██║██╔══██╗██╔════╝██╔════╝
+██║░░░░░██║░░░██║██╔██╗██║███████║██████╔╝  █████╗░░██║░░╚═╝██║░░░░░██║██████╔╝╚█████╗░█████╗░░
+██║░░░░░██║░░░██║██║╚████║██╔══██║██╔══██╗  ██╔══╝░░██║░░██╗██║░░░░░██║██╔═══╝░░╚═══██╗██╔══╝░░
+███████╗╚██████╔╝██║░╚███║██║░░██║██║░░██║  ███████╗╚█████╔╝███████╗██║██║░░░░░██████╔╝███████╗
+╚══════╝░╚═════╝░╚═╝░░╚══╝╚═╝░░╚═╝╚═╝░░╚═╝  ╚══════╝░╚════╝░╚══════╝╚═╝╚═╝░░░░░╚═════╝░╚══════╝
+    \____, Lunar Eclipse \ Future of Universal ScriptHub
+    \____, Founded by LiterallyBacon | Found in: 09/30/23, 8:30pm
+    \____, Do not copy this or you will be PERMANENTALY BANNED FROM LUNAR ECLIPSE!
+--]]
+
